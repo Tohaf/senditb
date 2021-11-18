edce9c4558d70ba3a629108890fb50b637a784eb
unknown <Toheebafolabi17@gmail.com>
2021-11-18 01:25:50 -0800

    make it better

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('connected to Mongoose'));  
 
+router.get('/', (req, res)=>{
+    res.send('wellcome');
+})
+
 
 
 
